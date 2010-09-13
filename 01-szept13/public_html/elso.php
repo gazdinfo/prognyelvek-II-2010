0d567fe7af85ecede11e078d6c71b4a737e98eb6
Marton Juhasz <juhasz.marci@gmail.com>
2010-09-13 17:33:13 +0200

    valtozo deklaralas es kiiratas

--- 01-szept13/public_html/elso.php
+++ 01-szept13/public_html/elso.php
@@ -3,6 +3,11 @@
 <?php
   // print ("<meta name=\"content\" content=\"text/html\" charset=\"utf-8\">"); // idezojelek levedve, igy mar mukodik
   print ('<meta name="content" content="text/html" charset="utf-8">'); // igy kicsit szebb, aposztroffal idezojel helyett
+  
+  $a = 5; // 'a' valtozo deklaralasa, egybol erteket is adunk neki
+          // PHP gyengen tipusos -> nem kell megadni a valtozo tipusat, kitalaja magatol
+          
+  print $a; // 'a' valtozo kiiratasa
 
   // phpinfo();
   
