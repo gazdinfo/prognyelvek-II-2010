aaaec9031334ea854efaed0c94116fec9f7b497c
Marton Juhasz <juhasz.marci@gmail.com>
2010-09-13 18:22:24 +0200

    orai tablazatos pelda egy egyedi megoldasa

--- 01-szept13/public_html/elso.php
+++ 01-szept13/public_html/elso.php
@@ -1,5 +1,3 @@
-
-
 <?php
   // phpinfo();
   
@@ -11,6 +9,32 @@
 
   // print ("<meta name=\"content\" content=\"text/html\" charset=\"utf-8\">"); // idezojelek levedve, igy mar mukodik
   print ('<meta name="content" content="text/html" charset="utf-8">'); // igy kicsit szebb, aposztroffal idezojel helyett
+?>
+
+<table border="1">
+  <?php
+  $i = 1;
+  while (true) {  // hehe vegtelen ciklus...
+    print  "<tr><td>";
+    print $i;
+    print "</td><td>";
+    print gettype($i);
+    print "</td></tr>";
+    
+    if (is_int($i)) { // irdatlan csunya if kupac...
+      $i = 1.1;
+    } elseif (is_double($i)) {
+      $i = 'I\'m a string!';
+    } elseif (is_string($i)) {
+      $i = true;
+    } else {
+      break; // ha mar minden megvan, akkor ne felejtsunk el kilepni az egyebkent vegtelen ciklusbol :)
+    }
+  }
+  ?>
+</table>
+
+<?php
   
   $a = 5; // 'a' valtozo deklaralasa, egybol erteket is adunk neki
           // PHP gyengen tipusos -> nem kell megadni a valtozo tipusat, kitalaja magatol
