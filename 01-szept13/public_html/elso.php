c1c6e348349322e0609e88b973ee741051a44fd3
Marton Juhasz <juhasz.marci@gmail.com>
2010-09-13 17:43:48 +0200

    valtozo kiiratasa tobb modon, string osszefuzes

--- 01-szept13/public_html/elso.php
+++ 01-szept13/public_html/elso.php
@@ -1,14 +1,6 @@
 
 
 <?php
-  // print ("<meta name=\"content\" content=\"text/html\" charset=\"utf-8\">"); // idezojelek levedve, igy mar mukodik
-  print ('<meta name="content" content="text/html" charset="utf-8">'); // igy kicsit szebb, aposztroffal idezojel helyett
-  
-  $a = 5; // 'a' valtozo deklaralasa, egybol erteket is adunk neki
-          // PHP gyengen tipusos -> nem kell megadni a valtozo tipusat, kitalaja magatol
-          
-  print $a; // 'a' valtozo kiiratasa
-
   // phpinfo();
   
   // egy sor kommentezese
@@ -16,6 +8,26 @@
   /*
    blokk kommentezese
   */
+
+  // print ("<meta name=\"content\" content=\"text/html\" charset=\"utf-8\">"); // idezojelek levedve, igy mar mukodik
+  print ('<meta name="content" content="text/html" charset="utf-8">'); // igy kicsit szebb, aposztroffal idezojel helyett
+  
+  $a = 5; // 'a' valtozo deklaralasa, egybol erteket is adunk neki
+          // PHP gyengen tipusos -> nem kell megadni a valtozo tipusat, kitalaja magatol
+          
+  print $a; // 'a' valtozo kiiratasa
+  
+  // jozan paraszti esszel osszerakott kiiratas, ami nem mukodik...
+  print "$a erteke =";
+  print $a;
+  print "<br />"; // sortores beillesztese
+  
+  // kicsit szebb megoldas
+  print "\$a erteke = $a";
+  print "<br />"; // sortores beillesztese
+  
+  // az altalaban hasznalt megoldas, string osszefuzessel
+  print '$a = ' . $a . "<br />";
   
   echo "Hello Vilag!"; // print helyett echo
 ?>
